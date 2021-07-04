3b96db899523860e89fc250a2ef4e3074d77432d
Anjola Adeyemi <47359428+Anjola85@users.noreply.github.com>
2021-07-04 14:05:17 -0400

    This file should be run separately.
    Run file with the required packages installed.

---
+++ asyncActions.js
@@ -0,0 +1,75 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+// ACTIONS
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_SUCCESS";
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+};
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+};
+
+// REDUCER
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return { ...state, loading: true };
+    case FETCH_USERS_SUCCESS:
+      return { loading: false, users: action.payload, error: "" };
+    case FETCH_USERS_FAILURE:
+      return { loading: false, users: [], error: action.payload };
+  }
+};
+
+// the thunk middleware helps action creators return a function instead of an object.
+// define async action creator
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest()); //set loading to true.
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        // response.data is the array of users
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        // error.message is the error description
+        dispatch(fetchUsersFailure(error.message));
+      });
+  };
+};
+
+//REDUX STORE
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+// finally
+store.subscribe(() => {
+  //listener that logs state once it is changed
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
